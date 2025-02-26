d554f09a7ca3e3a9194b573bcebacf2cd8bf3115
mykhailodrahan <mykhailodrahan@gamil.com>
2025-02-26 21:14:38 +0200

    feat(backend): add EF Core with Book model and migrations

---
+++ backend/BookApi/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookApi.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+    }
+}
